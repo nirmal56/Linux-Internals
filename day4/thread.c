a654d759ae3675aba8a56c2899334612465f90b4
nirmal56 <nirmal.patel@einfochips.com>
2022-03-10 10:56:22 +0530

    thread 1 prog added day4

--- day4/thread.c
+++ day4/thread.c
@@ -6,23 +6,29 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-pthread_t tid;
+pthread_t tid,tid2;
 
 void *thread1(void *arg){
+    for(int i=0;i<500;i++)
     printf("thread 1 executing\n");
     return NULL;
 }
-
+void *thread2(void *arg){
+    for(int i=0;i<500;i++)
+    printf("thread 2 executing\n");
+    return NULL;
+}
 int main(void){
+    getchar();
     int ret = pthread_create(&tid,NULL,thread1,NULL);
-    if(ret){
+    int ret2 = pthread_create(&tid2,NULL,thread2,NULL);
+    getchar();
+    if(ret || ret2){
         printf("cant creat\n");
-
     }
     else{
-        
         printf("created\n");
-        
         pthread_join(tid,NULL);
+        pthread_join(tid2,NULL);
     }
 }
