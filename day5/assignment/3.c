0f73fbc94ade0aab9b783967a9cd2298e57b06ad
nirmal56 <nirmal.patel@einfochips.com>
2022-03-15 07:58:24 +0530

    assignment all bugs fixed

--- day5/assignment/3.c
+++ day5/assignment/3.c
@@ -2,19 +2,17 @@
 3. A program that replaces old program with new program data and is expected to display the currently running processes in a 
 hierarchical tree format.
 */
-
 #include<stdio.h>
-#include<fcntl.h>
-#include<sys/types.h>
-#include<sys/stat.h>
 #include<unistd.h>
-#include<stdlib.h>
+
+
 
 int main(){
-    // int ret = execl("/home/nirmal/user/Linux-Internals/day5/assignment/1.c","vim","1.c",NULL);
-    // if(ret == -1){
-    //     printf("error\n");
-    // }
-
-    int retu = execl("pstree",NULL);
+printf("I'm going to open a info1.txt from vim\n");
+// int ret = execl("/usr/bin/vim", "vim","info1.txt", 0);
+int ret = execl("/usr/bin/pstree", "pstree", 0, 0);
+if(ret == -1){
+printf("execl returned error %d\n", ret);
+}
+return 0;
 }
