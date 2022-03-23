d0c83b4569bcb90d2911a15199fb46e7d8fad42f
nirmal56 <nirmal.patel@einfochips.com>
2022-03-23 10:19:03 +0530

    makefile done

---
+++ day12/mkfile1/calc_sub.c
@@ -0,0 +1,3 @@
+double sub(double a,double b){
+    return (a-b);
+}
---
+++ day12/mkfile1/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#include"calc_mean.h"
+
+int main(int argc,int argv[]){
+    double v1,v2,m1,m2;
+
+    v1=5.2;
+    v2=7.9;
+
+    m1 = mean(v1,v2);
+    m2 = sub(v1,v2);
+
+    printf("the mean of %3.3f and %3.2f is %3.2f\n",v1,v2,m1);
+    printf("the subtraction of %3.3f and %3.2f is %3.2f\n",v1,v2,m2);
+    return 0;
+}
