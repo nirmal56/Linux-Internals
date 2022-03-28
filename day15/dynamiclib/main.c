33c8cf8932e8b51f072dc842b61c0cbc57a1e736
nirmal56 <nirmal.patel@einfochips.com>
2022-03-28 10:56:30 +0530

    added day15

--- day15/dynamiclib/main.c
+++ day15/dynamiclib/main.c
@@ -3,15 +3,16 @@
 #include"calc_mean.h"
 
 int main(int argc,int argv[]){
-    double V1,v2,m1,m2;
+    double v1,v2,m1,m2;
 
     v1=5.2;
-    v2=8.9;
+    v2=7.9;
 
-    m1 = mean(V1,v2);
-    m2 = sub(V1,v2);
 
-    printf("the mean of %3.3f and %3.2f is %3.2f\n",V1,v2,m1);
-    printf("the subtraction of %3.3f and %3.2f is %3.2f\n",V1,v2,m2);
+    m1 = mean(v1,v2);
+    m2 = sub(v1,v2);
+
+    printf("the mean of %3.3f and %3.2f is %3.2f\n",v1,v2,m1);
+    printf("the subtraction of %3.3f and %3.2f is %3.2f\n",v1,v2,m2);
     return 0;
 }
